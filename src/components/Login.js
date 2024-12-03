5be42e579fe06b0568a45a0e4f3b8ad849cb8011
Victor Peinado <vpeinado263@gmail.com>
2024-12-03 14:25:06 -0300

    Crear estructura basica de formulario

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+function Login() {
+    return (
+        <form>
+            <input type="email" name="email"/>
+            <input type="password" name="password"/>
+            <button type="submit">Ingresar</button>
+        </form>
+    )
+}
+
+export default Login;
