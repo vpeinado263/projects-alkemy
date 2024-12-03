84b1e89f1f67b4f1d0d711dc3667707c35bc45ab
Victor Peinado <vpeinado263@gmail.com>
2024-12-03 17:26:57 -0300

    Cofigurar Hooks useHistory

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,4 +1,11 @@
+import { useHistory } from 'react-router-dom';
+
 function Listado () {
+    const history = useHistory();
+    const token = localStorage.getItem('item');
+    if(token === null) {
+        history.push('/');
+    }
     return (
         <h2>Componente Listado</h2>
     )
