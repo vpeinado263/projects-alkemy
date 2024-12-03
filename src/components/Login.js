0d97f4eaaa505b385eb0e4840cce3f534f1c204a
Victor Peinado <vpeinado263@gmail.com>
2024-12-03 15:52:47 -0300

    Implimentar funcion de tipo Handler

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,33 @@
 function Login() {
+    const submitHandler = e => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        console.log(regexEmail.test(email));
+
+        if(email === '' || password === '') {
+            console.log('Campos vacios');
+            return;
+        }
+        if(email !== '' && !regexEmail.test(email)) {
+            console.log('Direccion de correo Invalida');
+            return;
+        }
+        if(email !== 'CineOnline12@gmail.com' || password !== 'gratis') {
+            console.log('Credenciales Invalidad');
+            return;
+        }
+        console.log('Información lista para Enviar');
+    }
     return (
         <>
         <h2>Ingresar a Cuenta</h2>
-        <form>
+        <form onSubmit={submitHandler}>
             <label>
-                <span>Correo Electronico</span> <br />
-                <input type="email" name="email" />
+                <span>Correo Electrónico</span> <br />
+                <input type="text" name="email" />
             </label>
             <br />
             <label>
