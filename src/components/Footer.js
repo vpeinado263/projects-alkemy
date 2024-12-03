986e91bddb9be9ee38eb4f1d0979782b5e73c6ae
Victor Peinado <vpeinado263@gmail.com>
2024-12-03 20:20:55 -0300

    Implementar estructura basica de Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return(
+        <footer>
+            <nav>
+                <ul><a href="https://imstagram"></a></ul>
+            </nav>
+            <p>Coyrigyh</p>
+        </footer>
+    )
+}
+
+export default Footer;
