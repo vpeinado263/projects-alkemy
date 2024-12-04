872d24d2cda0b1190d79f7da27f3f8378b879b5e
Victor Peinado <vpeinado263@gmail.com>
2024-12-03 21:16:55 -0300

    Referncia componentes footer y header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Switch, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import Listado from './components/Listado';
 import Login from "./components/Login";
 
 function App() {
   return (
     <>
+    <Header/>
     <Switch>
      <Route exact patch="/" component={Login} />
      <Route patch="/listado" component={Listado} />
     </Switch>
+    <Footer/>
     </>
   );
 }
