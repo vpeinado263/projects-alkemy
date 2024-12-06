a2f6a9333e21d4539405240186555a056cb30bc0
Victor Peinado <vpeinado263@gmail.com>
2024-12-06 16:47:28 -0300

    Editar archivo App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Listado from './components/Listado';
 import Login from './components/Login';
-
 import './css/bootstrap.min.css';
 
 function App() {
+
   return (
     <>
     <Header/>
-    
-    {/* <div className='container mt-3'> */}
-     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/listado" element={<Listado />} />
-     </Routes>
+  
+    <div className='container mt-3'>
 
-    <Footer/>
+      <Routes>
+        <Route path="/"/>
+        <Route path="/listado" element={<Listado />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <Footer/>
    
-   
+    </div>
     </>
   );
 }
