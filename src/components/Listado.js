849531d30293927a3bc3c47a933430200a079832
Victor Peinado <vpeinado263@gmail.com>
2024-12-06 02:25:16 -0300

    Crear rrenderizado condicional

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,27 +1,25 @@
-import { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 function Listado () {
-    const history = useHistory();
-    
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if(token === null) {
-            history.push('/');
-        }
-    }, []);
-   
+    let  token = localStorage.getItem('token')
+       
     return (
-        <div className="row">
-            <div className="col-3">Peli-1</div>
-            <div className="col-3">Peli-2</div>
-            <div className="col-3">Peli 3</div>
-            <div className="col-3">Peli 4</div>
-            <div className="col-3">Peli-5</div>
-            <div className="col-3">Peli-6</div>
-            <div className="col-3">Peli 7</div>
-            <div className="col-3">Peli 8</div>
+        <>
+
+        { !token && <Redirect to="/" />}
+
+        <div className="row"> 
+         <div className="col-3">
+             <div className="card">
+                 <img src='...' className='card-img-top'  alt='...'/>
+                 <div className='card-body'></div>
+                 <h5 className='card-title'>Movie title</h5>
+                 <p className='card-text'>gwwwwwwwwwwwwwwwwwwwfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff</p>
+                 <Link href="/" className='btn btn-primary'>ver detalles</Link>
+             </div>
+         </div>
         </div>
+        </>
     )
 }
 
