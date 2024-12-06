bbb27d9577252091bafe79c24d0cedfcd71dc411
Victor Peinado <vpeinado263@gmail.com>
2024-12-06 19:02:52 -0300

    Agregar estiloscomplemetarios

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -27,13 +27,13 @@ function Listado  ()  {
         {
           moviesList.map((oneMovie, idx) => {
             return (
-              <div className="col-3" key={idx}>
-                <div className="card my-4">
-                  <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.backdrop_path}`} className="card-img-top"  alt="..."/>
+              <div className="col-4 col-sm-4" key={idx}>
+                <div className="card my-4 shadow-sm">
+                  <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.backdrop_path}`} className="card-img-top"  alt={oneMovie.title}/>
                   <div className="card-body"></div>
-                  <h5 className="card-title">{ oneMovie.title.substring(0, 30) }...</h5>
-                  <p className="card-text">{ oneMovie.overview.substring(0, 100) }...</p>
-                  <Link to="/" className="btn btn-secondary">ver detalles</Link>
+                  <h5 className="card-title text-truncate">{ oneMovie.title.substring(0, 30) }...</h5>
+                  <p className="card-text text-truncate">{ oneMovie.overview.substring(0, 100) }...</p>
+                  <Link to="/" className="btn btn-secondary btn-sm">ver detalles</Link>
                 </div>
               </div>
             )
