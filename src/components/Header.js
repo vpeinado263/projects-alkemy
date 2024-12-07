d8721af4c310a68fa16ffee46c48baaa78038c24
Victor Peinado <vpeinado263@gmail.com>
2024-12-07 00:41:00 -0300

    Modificar componente Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,24 +6,15 @@ function Header () {
     return (
         <header>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="container">
-                    <ul>
+                <div className="container d-flex justify-content-between align-items-center">
+
+                        {/*Logo*/}
                         <Link to="/" className="navbar-brand">
                             FilmMovie
                         </Link>
-                        <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                        </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul  className="navbar-nav ms-auto">
+
+                        {/*Barra de navegacion*/}
+                        <ul  className="navbar-nav d-flex flex-row gap-3">
                         <li className="nav-item">
                                 <Link className="nav-link" to="/login">
                                     Home
@@ -34,9 +25,8 @@ function Header () {
                                     Listado
                                 </Link>
                             </li>
-                        </ul>
-                    </div>
                     </ul>
+                    
                 </div>
                 <Buscador />
             </nav>
