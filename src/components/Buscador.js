30abefbf64b158b11ac488f5e2dfb893ee60f499
Victor Peinado <vpeinado263@gmail.com>
2024-12-07 02:16:41 -0300

    Implementar componente Buscador

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -1,6 +1,30 @@
+import swAlert from 'sweetalert2';
+
 function Buscador() {
+    const submitHandler = e => {
+
+        e.preventDefault()
+
+        const keyword = e.currentTarget.keyword.value;
+        console.log(keyword)
+
+        if( keyword.length === 0 ) {
+            swAlert.fire(
+                {
+                    title: 'Palabra incorrecta',
+                    html: 'Tienes que escribir una palabra clave.',
+                    icon: 'error'
+                } );
+        }
+    }
+
   return (
-    <h1>Buscador</h1>
+    <form className="d-flex align-items-center" onSubmit={submitHandler}>
+        <label className="form-label mb-0 mx-2">
+            <input className="form-control" type="text" name="keyword" placeholder="Escribe una palabra clave..." required />
+        </label>
+        <button className="btn btn-success" >Buscar</button>
+    </form>
   )
 }
 
