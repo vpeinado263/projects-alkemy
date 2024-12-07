c7934dde4483224edcb1a970299e88f541eeda4a
Victor Peinado <vpeinado263@gmail.com>
2024-12-07 14:26:46 -0300

    Implementar menejo de errores

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -5,22 +5,30 @@ function Buscador() {
 
         e.preventDefault()
 
-        const keyword = e.currentTarget.keyword.value;
+        const keyword = e.currentTarget.keyword.value.trim();
         console.log(keyword)
 
         if( keyword.length === 0 ) {
             swAlert.fire(
                 {
                     title: 'Palabra incorrecta',
-                    html: 'Tienes que escribir una palabra clave.',
+                    html: 'Tienes que escribir una palabra o letras claves.',
                     icon: 'error'
                 } );
+        } else if ( keyword.length < 4 ) {
+            swAlert.fire(
+                {
+                    title: 'Faltan caracteres',
+                    html: 'Tiene que escribir mas de 4 caracteres',
+                    icon: 'error'
+                }
+            )
         }
     }
 
   return (
     <form className="d-flex align-items-center" onSubmit={submitHandler}>
-        <label className="form-label mb-0 mx-4">
+        <label className="form-label mb-0 mx-2">
             <input className="form-control" type="text" name="keyword" placeholder="Escribe una palabra clave..."  />
         </label>
         <button className="btn btn-dark" >Buscar</button>
