c86e753a62351b929e934983502603f8da6ce754
Victor Peinado <vpeinado263@gmail.com>
2024-12-09 18:40:46 -0300

    Implementar manejo de localStorage

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ import './css/bootstrap.min.css';
 
 function App() {
 
+  const favMovies = localStorage.getItem('favs');
+  let tempMoviesInFavs;
+
+  if (favMovies === null) {
+    tempMoviesInFavs = [];
+  } else {
+    tempMoviesInFavs = JSON.parse(favMovies);
+  }
+  console.log(tempMoviesInFavs);
+
   const addOrRemoveFromFavs = (e) => {
     const btn = e.currentTarget;
     const parent = btn.parentElement;
@@ -19,9 +29,25 @@ function App() {
     const overview = parent.querySelector('p').innerText;
     
     const movieData = {
-      imgURL, title, overview
+      imgURL, title, overview,
+      id: btn.dataset.movieId
     }
-    console.log(movieData);
+    let movieIsInArray = tempMoviesInFavs.find(oneMovie => {
+      return oneMovie.id === movieData.id
+    })
+    if(!movieIsInArray) {
+      tempMoviesInFavs.push(movieData);
+      localStorage.setItem('favs', JSON.stringify(tempMoviesInFavs));
+      console.log('Seagregó la pelicula');
+    } else {
+      let movieLeft = tempMoviesInFavs.filter(oneMovie => {
+        return oneMovie.id !== movieData.id
+      });
+      localStorage.setItem('favs', JSON.stringify(movieLeft));
+      console.log('Se elininó la película');
+    }
+
+   
   }
 
   return (
