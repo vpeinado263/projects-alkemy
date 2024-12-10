e8275ea301fd3ddf0806d4bea101ae5095bab519
Victor Peinado <vpeinado263@gmail.com>
2024-12-10 05:37:25 -0300

    Crear componente Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home () {
+  return (
+    <div className="text-center">
+      <h1>Bienvenidos a FilmMovie</h1>
+      <p>Iniciar sesión o explora nuestro contenido</p>
+    </div>
+  );
+}
+
+export default Home;
