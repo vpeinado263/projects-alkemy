c73c7223e4f68e9877b38f7da3aa1e4a7ed99b76
Victor Peinado <vpeinado263@gmail.com>
2024-12-10 03:50:07 -0300

    Importar Estados en Componente

--- src/components/Favoritos.js
+++ src/components/Favoritos.js
@@ -1,4 +1,17 @@
+import { useState, useEffect } from "react";
+
 function Favoritos () {
+    const [ favorites, setFavorites ] = useState([]);
+    useEffect(() => {
+        const favsInLocal = localStorage.getItem('favs');
+       
+
+        if (favsInLocal != null) {
+            const favsArray = JSON.parse(favsInLocal);
+            console.log(favsArray)
+        }
+
+    }, [])
   return (
     <h2>Sección Favoritos</h2>
   )
