84099f637640e1d5759a750dff9acfdf83b9a9e3
Victor Peinado <vpeinado263@gmail.com>
2024-12-10 03:59:27 -0300

    Implementar Logica de Favoritos

--- src/components/Favoritos.js
+++ src/components/Favoritos.js
@@ -8,12 +8,38 @@ function Favoritos () {
 
         if (favsInLocal != null) {
             const favsArray = JSON.parse(favsInLocal);
-            console.log(favsArray)
+            setFavorites(favsArray);
         }
 
     }, [])
   return (
-    <h2>Sección Favoritos</h2>
+    <>
+     <h2>Sección Favoritos</h2>
+     <div className="row"> 
+        {/*Estructura Base*/}
+        {
+          favorites.map((oneMovie, idx) => {
+            return (
+              <div className="col-4 col-sm-4" key={idx}>
+                <div className="card my-4 shadow-sm">
+                  <img src={oneMovie.imgURL} className="card-img-top"  alt={oneMovie.title}/>
+                  {/* <button 
+                  className="favourite-btn"
+                  onClick={(e) => props.addOrRemoveFromFavs(e, oneMovie)}
+                  data-movie-id={oneMovie.id}
+                  >🖤</button> */}
+                  <div className="card-body"></div>
+                  <h5 className="card-title text-truncate">{ oneMovie.title.substring(0, 30) }...</h5>
+                  <p className="card-text text-truncate">{ oneMovie.overview.substring(0, 100) }...</p>
+                  {/* <Link to={`/detalle?movieID=${oneMovie.id}`} className="btn btn-secondary btn-sm">ver detalles</Link> */}
+                </div>
+              </div>
+            )
+          })
+        }
+        </div>
+    </>
+   
   )
 }
 
